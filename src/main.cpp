265e50ccc270696a45695719710983a9a308a803
Kevin Lou <klou23@kentdenver.org>
2019-12-15 20:57:31 -0700

    asdf

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,6 @@
 /*----------------------------------------------------------------------------*/
 
 
-/*----------------------------------------------------------------------------*/
-/*                               Program Notes                                */
-/*    Claw motor encoder needs to be set to correct values                    */
-/*    Motor speeds may need to be changed                                     */
-/*    Rollers are currently spinning constantly, this may need to be changed  */
-/*    Autonomous code needs to be added                                       */
-/*----------------------------------------------------------------------------*/
 #include "vex.h"
 
 using namespace vex;
@@ -29,14 +22,165 @@ competition Competition;
 //global instances of motors and other devices here
 //vex::motor leftFrontDrive(vex::PORT12, vex::gearSetting::ratio18_1, false);
 //vex::motor rightFrontDrive(vex::PORT11, vex::gearSetting::ratio18_1, true);
-vex::motor leftBackDrive(vex::PORT2, vex::gearSetting::ratio18_1, false);
-vex::motor rightBackDrive(vex::PORT7, vex::gearSetting::ratio18_1, true);
-vex::motor rightLiftMotor(vex::PORT1, vex::gearSetting::ratio36_1, false);
-vex::motor leftLiftMotor(vex::PORT20, vex::gearSetting::ratio36_1, true);
-vex::motor clawMotor(vex::PORT21, vex::gearSetting::ratio18_1, false);
-vex::motor leftRollerMotor(vex::PORT12, vex::gearSetting::ratio18_1, false);
-vex::motor rightRollerMotor(vex::PORT3, vex::gearSetting::ratio18_1, true);
+vex::motor leftDrive(vex::PORT1, vex::gearSetting::ratio18_1, false);
+vex::motor rightDrive(vex::PORT2, vex::gearSetting::ratio18_1, true);
+vex::motor lift1(vex::PORT3, vex::gearSetting::ratio36_1, false);
+vex::motor lift2(vex::PORT4, vex::gearSetting::ratio36_1, true);
+vex::motor lift3(vex::PORT5, vex::gearSetting::ratio36_1, true);
+vex::motor leftRoller(vex::PORT6, vex::gearSetting::ratio36_1, false);
+vex::motor rightRoller(vex::PORT7, vex::gearSetting::ratio36_1, false);
+vex::motor shifter(vex::PORT8, vex::gearSetting::ratio36_1, false);
 vex::controller controller1 = vex::controller();
+
+/*---------------------------------------------------------------------------*/
+/*                             Variables, Arrays                             */
+/*---------------------------------------------------------------------------*/
+
+/*
+  Auton List:
+  1. Blue 4 cube line (4 points)
+  2. Red 4 cube line (4 points)
+  3. Blue 1 cube and 4 cube stack (5 points)
+  4. Red 1 cube and 4 cube stack (5 points)
+*/
+
+int Auton = 1;
+
+motor motorArray[8] = {leftDrive, rightDrive, lift1, lift2, lift3, leftRoller, rightRoller, shifter};
+std::string motorNames[8] = {"Left Drive", "Right Drive", "Lift 1", "Lift 2", "Lift 3", "Left Roller", "Right Roller", "Shifter"};
+
+double rightDriveVel = 0;
+double leftDriveVel = 0;
+
+/*---------------------------------------------------------------------------*/
+/*                                Functions                                  */
+/*---------------------------------------------------------------------------*/
+
+void controllerDrive(void){
+  rightDriveVel = controller1.Axis2.value() * 0.8;
+  leftDriveVel = controller1.Axis3.value() * 0.8;
+  rightDrive.spin(directionType::fwd, rightDriveVel, velocityUnits::pct);
+  leftDrive.spin(directionType::fwd, leftDriveVel, velocityUnits::pct);
+}
+
+void liftUp(double speed){
+  lift1.spin(directionType::fwd, speed, velocityUnits::pct);
+  lift2.spin(directionType::fwd, speed, velocityUnits::pct);
+  lift3.spin(directionType::fwd, speed, velocityUnits::pct);
+}
+
+void liftDown(double speed){
+  lift1.spin(directionType::rev, speed, velocityUnits::pct);
+  lift2.spin(directionType::rev, speed, velocityUnits::pct);
+  lift3.spin(directionType::rev, speed, velocityUnits::pct);
+}
+
+void liftStop(void){
+  lift1.stop(hold);
+  lift2.stop(hold);
+  lift3.stop(hold);
+}
+
+void liftUpFor(double time, double speed){
+  liftUp(speed);
+  wait(time, msec);
+  liftStop();
+}
+
+void liftDownFor(double time, double speed){
+  liftDown(speed);
+  wait(time, msec);
+  liftStop();
+}
+
+void drive(double time, double speed){
+  leftDrive.spin(directionType::fwd, speed, velocityUnits::pct);
+  rightDrive.spin(directionType::fwd, speed, velocityUnits::pct);
+  wait(time, msec);
+  leftDrive.stop();
+  rightDrive.stop();
+}
+
+void turnCW(double time, double speed){
+  leftDrive.spin(directionType::fwd, speed, velocityUnits::pct);
+  rightDrive.spin(directionType::rev, speed, velocityUnits::pct);
+  wait(time, msec);
+  leftDrive.stop();
+  rightDrive.stop();
+}
+
+void turnCCW(double time, double speed){
+  leftDrive.spin(directionType::rev, speed, velocityUnits::pct);
+  rightDrive.spin(directionType::fwd, speed, velocityUnits::pct);
+  wait(time, msec);
+  leftDrive.stop();
+  rightDrive.stop();
+}
+
+double testTemp(motor testMotor){
+  return testMotor.temperature();
+}
+
+bool isOverTemp(motor motorName, int temp){
+  bool returnValue = false;
+  if(motorName.temperature(temperatureUnits::fahrenheit) > temp){
+    returnValue = true;
+  }
+  return returnValue;
+}
+
+bool allMotorsOverTemp(motor motors[8], int temp){
+  bool returnValue = false;
+  for(int i = 0; i < 8; i++){
+    if(isOverTemp(motors[i], temp)){
+      returnValue = true;
+    }
+  }
+  return returnValue;
+}
+
+int circleColor(void){
+  int returnValue;
+  if(allMotorsOverTemp(motorArray, 150)){
+    returnValue = 0;
+  }else if(allMotorsOverTemp(motorArray, 125)){
+    returnValue = 30;
+  }else if(allMotorsOverTemp(motorArray, 100)){
+    returnValue = 60;
+  }else {
+    returnValue = 90;
+  }
+  return returnValue;
+}
+
+void rollerIntake(double speed){
+  leftRoller.spin(directionType::fwd, speed, velocityUnits::pct);
+  rightRoller.spin(directionType::fwd, speed, velocityUnits::pct);
+}
+
+void rollerExtake(double speed){
+  leftRoller.spin(directionType::fwd, speed, velocityUnits::pct);
+  rightRoller.spin(directionType::fwd, speed, velocityUnits::pct);
+}
+
+void rollerStop(void){
+  leftRoller.stop();
+  rightRoller.stop();
+}
+
+void shifterUp(void){
+  shifter.spin(directionType::fwd, 50, velocityUnits::pct);
+  wait(300, msec);
+  shifter.spin(directionType::fwd, 15, velocityUnits::pct);
+  wait(300, msec);
+  shifter.stop(hold);
+}
+
+void shifterDown(void){
+  shifter.spin(directionType::rev, 75, velocityUnits::pct);
+  wait(300, msec);
+  shifter.stop();
+}
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -52,190 +196,91 @@ void pre_auton(void) {
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  //Paste code in
-
-    //red
-    //lift up
-      rightLiftMotor.spin(directionType::fwd, 80, velocityUnits::pct);
-      leftLiftMotor.spin(directionType::fwd, 80, velocityUnits::pct); 
-      wait(500, msec);
-      rightLiftMotor.stop(hold);
-      leftLiftMotor.stop(hold);
-
-      int degrees = 350;
-
-      //claw close
-      clawMotor.rotateTo(-30, rotationUnits::deg, 20, velocityUnits::pct, false);
-
-      //drive forward
-      leftBackDrive.rotateFor(directionType::fwd, degrees, rotationUnits::deg, false);
-      rightBackDrive.rotateFor(directionType::fwd, degrees, rotationUnits::deg, false);
-      wait(degrees*5, msec);
-
-      //back away
-      leftBackDrive.spin(directionType::rev, 50, velocityUnits::pct);
-      rightBackDrive.spin(directionType::rev, 50, velocityUnits::pct);
-      wait(500, msec);
-      leftBackDrive.stop();
-      rightBackDrive.stop();
-
-      //turn
-      leftBackDrive.spin(directionType::fwd, 25, velocityUnits::pct);
-      rightBackDrive.spin(directionType::rev, 25, velocityUnits::pct);
-      wait(800, msec);
-      leftBackDrive.stop();
-      rightBackDrive.stop();
-
-      //spin rollers
-      leftRollerMotor.spin(directionType::fwd, 75, velocityUnits::pct);
-      rightRollerMotor.spin(directionType::fwd, 75, velocityUnits::pct);
-
-      //go forward
-      leftBackDrive.spin(directionType::fwd, 50, velocityUnits::pct);
-      rightBackDrive.spin(directionType::fwd, 50, velocityUnits::pct);
-      wait(1000, msec);
-      leftBackDrive.stop();
-      rightBackDrive.stop();
-
-      //turn right to head for goblet
-      leftBackDrive.spin(directionType::fwd, 25, velocityUnits::pct);
-      rightBackDrive.spin(directionType::rev, 25, velocityUnits::pct);
-      wait(1100, msec);
-      leftBackDrive.stop();
-      rightBackDrive.stop();
-
-      //go forward
-      leftBackDrive.spin(directionType::fwd, 50, velocityUnits::pct);
-      rightBackDrive.spin(directionType::fwd, 50, velocityUnits::pct);
-      wait(200, msec);
-      //reverse rollers
-      leftRollerMotor.spin(directionType::rev, 50, velocityUnits::pct);
-      rightRollerMotor.spin(directionType::rev, 50, velocityUnits::pct);
-      //continue forward
-      wait(1550, msec);
-      leftBackDrive.stop();
-      rightBackDrive.stop();
-
-      //lift down
-      rightLiftMotor.spin(directionType::rev, 100, velocityUnits::pct);
-      leftLiftMotor.spin(directionType::rev, 100, velocityUnits::pct); 
-      wait(1000, msec);
-      rightLiftMotor.stop(hold);
-      leftLiftMotor.stop(hold);
-
-      //lift up
-      rightLiftMotor.spin(directionType::fwd, 80, velocityUnits::pct);
-      leftLiftMotor.spin(directionType::fwd, 80, velocityUnits::pct); 
-      wait(1150, msec);
-      rightLiftMotor.stop(hold);
-      leftLiftMotor.stop(hold);
-
-      //turn right to get to goblet
-      leftBackDrive.spin(directionType::fwd, 25, velocityUnits::pct);
-      rightBackDrive.spin(directionType::rev, 25, velocityUnits::pct);
-      wait(900, msec);
-      leftBackDrive.stop();
-      rightBackDrive.stop();
-
-      //go forward
-      leftBackDrive.spin(directionType::fwd, 50, velocityUnits::pct);
-      rightBackDrive.spin(directionType::fwd, 50, velocityUnits::pct);
-      wait(950, msec);
-      leftBackDrive.stop();
-      rightBackDrive.stop();
-
-      //claw open
-      clawMotor.rotateTo(250, rotationUnits::deg, 100, velocityUnits::pct, true);
-
-      //back away
-      leftBackDrive.spin(directionType::rev, 50, velocityUnits::pct);
-      rightBackDrive.spin(directionType::rev, 50, velocityUnits::pct);
-      wait(1000, msec);
-      leftBackDrive.stop();
-      rightBackDrive.stop();
-
-      rightRollerMotor.stop();
-      leftRollerMotor.stop();
+  if(Auton == 1){
+    rollerIntake(80);
+    drive(1500, 50);
+    turnCCW(500, 50);
+    drive(1000,50);
+    shifterUp();
+    drive(250, -50);
+    shifterDown();
+  }else if(Auton == 2){
+    rollerIntake(80);
+    drive(1500, 50);
+    turnCW(500, 50);
+    drive(1000,50);
+    shifterUp();
+    drive(250, -50);
+    shifterDown();
+  }else if(Auton == 3){
+    rollerIntake(80);
+    drive(1000, 50);
+    liftUpFor(800, 80);
+    drive(500, 50);
+    liftDownFor(1000, 50);
+    turnCW(500, 50);
+    drive(1000, 50);
+    shifterUp();
+    drive(250, -50);
+    shifterDown();
+  }else if(Auton == 4){
+    rollerIntake(80);
+    drive(1000, 50);
+    liftUpFor(800, 80);
+    drive(500, 50);
+    liftDownFor(1000, 50);
+    turnCCW(500, 50);
+    drive(1000, 50);
+    shifterUp();
+    drive(250, -50);
+    shifterDown();
+  }
 }
+
+
 
 /*---------------------------------------------------------------------------*/
 /*                            User Control Task                              */
 /*---------------------------------------------------------------------------*/
 
 void usercontrol(void) {
-  bool clawClose = true;
-  double RightDriveVel;
-  double LeftDriveVel;
   // main execution loop for the user control program.
   while (1) {
     //drive
-    RightDriveVel = controller1.Axis2.value() * 0.8;
-    LeftDriveVel = controller1.Axis3.value() * 0.8;
-    //right drive
-    //rightFrontDrive.spin(directionType::fwd, RightDriveVel, velocityUnits::pct);
-    rightBackDrive.spin(directionType::fwd, RightDriveVel, velocityUnits::pct);
-    //left drive
-    //leftFrontDrive.spin(directionType::fwd, LeftDriveVel, velocityUnits::pct);
-    leftBackDrive.spin(directionType::fwd, LeftDriveVel, velocityUnits::pct);
+    controllerDrive();
 
     //lift
     if(controller1.ButtonR1.pressing()){
-      //lift up
-      rightLiftMotor.spin(directionType::fwd, 100, velocityUnits::pct);
-      leftLiftMotor.spin(directionType::fwd, 100, velocityUnits::pct);
+      liftUp(100);
     }else if(controller1.ButtonR2.pressing()){
-      //lift down
-      if(leftLiftMotor.position(rotationUnits::deg)>-30){
-        rightLiftMotor.spin(directionType::rev, 100, velocityUnits::pct);
-        leftLiftMotor.spin(directionType::rev, 100, velocityUnits::pct);
-      }else{
-        rightLiftMotor.spin(directionType::rev, 100, velocityUnits::pct);
-        leftLiftMotor.spin(directionType::rev, 100, velocityUnits::pct);
-      }
-      
+      liftDown(100);
     }else{
-      //lift stop
-      rightLiftMotor.stop(hold);
-      leftLiftMotor.stop(hold);
+      liftStop();
     }
 
-    //claw
-    if(controller1.ButtonUp.pressing()){
-      //claw close
-      clawClose=true;
-    }else if(controller1.ButtonDown.pressing()){
-      //claw open
-      clawClose=false;
-    }
-    if(clawClose){
-      clawMotor.rotateTo(-30, rotationUnits::deg, 20, velocityUnits::pct, false);
-    }else{
-      clawMotor.rotateTo(250, rotationUnits::deg, 100, velocityUnits::pct, false);
-    }
-
-    Brain.Screen.clearScreen();
-      Brain.Screen.setCursor(2, 2);
-      Brain.Screen.print(clawMotor.temperature(temperatureUnits::fahrenheit));
-      if(clawMotor.temperature(temperatureUnits::fahrenheit) > 140){
-        Brain.Screen.drawCircle(240, 120, 100, 0);
-      }else if(clawMotor.temperature(temperatureUnits::fahrenheit) > 120){
-        Brain.Screen.drawCircle(240, 120, 100, 39);
-      }else if(clawMotor.temperature(temperatureUnits::fahrenheit) > 100){
-        Brain.Screen.drawCircle(240, 120, 100, 55);
-      }else{
-        Brain.Screen.drawCircle(240, 120, 100, 95);
-      }
-
-    //rollers
+    //roller
     if(controller1.ButtonL1.pressing()){
-      leftRollerMotor.spin(directionType::fwd, 75, velocityUnits::pct);
-      rightRollerMotor.spin(directionType::fwd, 75, velocityUnits::pct);
+      rollerIntake(100);
     }else if(controller1.ButtonL2.pressing()){
-      rightRollerMotor.spin(directionType::rev, 75, velocityUnits::pct);
-      leftRollerMotor.spin(directionType::rev, 75, velocityUnits::pct);
+      rollerExtake(100);
     }else{
-      leftRollerMotor.stop();
-      rightRollerMotor.stop();
+      rollerStop();
+    }
+
+    //shifter
+    if(controller1.ButtonUp.pressing()){
+      shifterUp();
+    }else if(controller1.ButtonDown.pressing()){
+      shifterDown();
+    }
+
+    //motor temp stuff
+    Brain.Screen.drawCircle(212, 60, 50, circleColor());
+    for(int i = 0; i < 8 ; i++){
+      if(isOverTemp(motorArray[i], 125)){
+        Brain.Screen.setCursor(i+1, 1);
+        Brain.Screen.print(motorNames[i].c_str());
+      }
     }
 
     wait(20, msec); // Sleep the task to save resources
