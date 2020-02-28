4539f5ca18f2dbb2b1cf9f6b225e81cd173b860f
Grant Fitez <grantfitez@gmail.com>
2020-02-27 18:16:13 -0700

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 #include "vex.h"
 
+
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
@@ -16,35 +17,49 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 using namespace vex;
+extern brain Brain;
+
 
 // A global instance of competition
 competition Competition;
 
 // global instances of motors and other devices here
-vex::motor leftDrive(vex::PORT18, vex::gearSetting::ratio18_1, false);
-vex::motor rightDrive(vex::PORT19, vex::gearSetting::ratio18_1, true);
-vex::motor rightLift(vex::PORT17, vex::gearSetting::ratio36_1, true);
-vex::motor leftLift(vex::PORT16, vex::gearSetting::ratio36_1, true);
-vex::motor leftRoller(vex::PORT1, vex::gearSetting::ratio36_1, false);
-vex::motor rightRoller(vex::PORT3, vex::gearSetting::ratio36_1, true);
-vex::motor shifter1(vex::PORT8, vex::gearSetting::ratio36_1, true);
-vex::motor shifter2(vex::PORT11, vex::gearSetting::ratio36_1, true);
+vex::motor leftDrive(vex::PORT10, vex::gearSetting::ratio18_1, false);
+vex::motor rightDrive(vex::PORT1, vex::gearSetting::ratio18_1, true);
+vex::motor rightLift(vex::PORT11, vex::gearSetting::ratio36_1, true);
+vex::motor leftLift(vex::PORT16, vex::gearSetting::ratio36_1, false);
+vex::motor leftRoller(vex::PORT20, vex::gearSetting::ratio36_1, false);
+vex::motor rightRoller(vex::PORT13, vex::gearSetting::ratio36_1, true);
+vex::motor shifter1(vex::PORT9, vex::gearSetting::ratio36_1, false);
+vex::motor shifter2(vex::PORT3, vex::gearSetting::ratio36_1, true);
 vex::controller controller1 = vex::controller(controllerType::primary);
 vex::controller controller2 = vex::controller(controllerType::partner);
-vex::inertial gyroscope(vex::PORT12);
+vex::inertial gyroscope(vex::PORT8);
+
+vex::triport ThreeWirePort(vex::PORT22);
+vex::pot shifterPot(ThreeWirePort.B);
+
+
+
+//triport ThreeWirePort = vex::triport( vex::PORT22 );
+
+
 
 //global variable
 double leftDriveSpeed;
 double rightDriveSpeed;
-
+int shifterDown = 4095;
+int shifterUp=1860;
+float shifterKp=-0.043;//negative because sensor is reversed
+int totalDistance = shifterDown-shifterUp;
 /*---------------------------------------------------------------------------*/
 /*                                 Functions                                 */
 /*---------------------------------------------------------------------------*/
 
 void controllerDrive(void){
   if(!controller2.ButtonX.pressing()){
-    leftDriveSpeed = ((controller1.Axis3.value()/100)*(controller1.Axis3.value()/100)) * 100;
-    rightDriveSpeed = ((controller1.Axis2.value()/100)*(controller1.Axis2.value()/100)) * 100;
+    leftDriveSpeed = controller1.Axis3.value();
+    rightDriveSpeed = controller1.Axis2.value();
     rightDrive.spin(directionType::fwd, rightDriveSpeed, velocityUnits::pct);
     leftDrive.spin(directionType::fwd, leftDriveSpeed, velocityUnits::pct);
   }else{
@@ -53,14 +68,15 @@ void controllerDrive(void){
   }
 }
 
+
 void rollerIntake(){
   leftRoller.spin(directionType::fwd, 80, velocityUnits::pct);
   rightRoller.spin(directionType::fwd, 80, velocityUnits::pct);
 }
 
 void rollerExtake(){
-  leftRoller.spin(directionType::fwd, 80, velocityUnits::pct);
-  rightRoller.spin(directionType::fwd, 80, velocityUnits::pct);
+  leftRoller.spin(directionType::rev, 80, velocityUnits::pct);
+  rightRoller.spin(directionType::rev, 80, velocityUnits::pct);
 }
 
 void rollerStop(){
@@ -69,13 +85,19 @@ void rollerStop(){
 }
 
 void manualShifterUp(void){
-  shifter1.spin(directionType::fwd, 40, velocityUnits::pct);
-  shifter2.spin(directionType::fwd, 40, velocityUnits::pct);
+  // int traveledDistance = shifterDown - shifterPot.angle();
+  // float distanceError = totalDistance - traveledDistance;
+  //float speed=shifterKp* distanceError;
+  int distanceError = shifterUp-shifterPot.value(vex::analogUnits::range12bit);
+  int speed = distanceError * shifterKp;
+  shifter1.spin(directionType::fwd, speed, velocityUnits::pct);
+  shifter2.spin(directionType::fwd, speed, velocityUnits::pct);
 }
 
 void manualShifterDown(void){
-  shifter1.spin(directionType::rev, 40, velocityUnits::pct);
-  shifter2.spin(directionType::rev, 40, velocityUnits::pct);
+  float speed=80;
+  shifter1.spin(directionType::rev, speed, velocityUnits::pct);
+  shifter2.spin(directionType::rev, speed, velocityUnits::pct);
 }
 
 void autonShifterUp(void){
@@ -125,16 +147,19 @@ void turnCCW(double amount, double speed){
   leftDrive.stop(hold);
 }
 
-void driveFwd(int amount, double speed){
-  Drivetrain.setDriveVelocity(speed, percent);
-  Drivetrain.driveFor(forward, amount, inches);
-}
 
-void driveRev(int amount, double speed){
-  Drivetrain.setDriveVelocity(speed, percent);
-  Drivetrain.driveFor(reverse, amount, inches);
+void lift(int speed){
+  leftLift.spin(vex::directionType::fwd, speed, vex::percentUnits::pct);
+  rightLift.spin(vex::directionType::fwd, speed, vex::percentUnits::pct);
 }
-
+void driveDist(float amount, float speed){
+  rightDrive.rotateFor(amount,rotationUnits::deg,speed,velocityUnits::pct, false);
+  leftDrive.rotateFor(amount,rotationUnits::deg,speed,velocityUnits::pct);
+}
+void liftTo(int height, int speed){
+  rightLift.rotateTo(-height, rotationUnits::deg, speed, velocityUnits::pct, false);
+  leftLift.rotateTo(height, rotationUnits::deg, speed, velocityUnits::pct);
+}
 /*---------------------------------------------------------------------------*/
 /*                                 Pre-Auton                                 */
 /*---------------------------------------------------------------------------*/
@@ -147,6 +172,7 @@ void pre_auton(void) {
   }
   leftDrive.setBrake(coast);
   rightDrive.setBrake(coast);
+
 }
 
 /*---------------------------------------------------------------------------*/
@@ -160,6 +186,27 @@ void autonomous(void) {
   USE THIS PROGRAM FOR AUTON, USE THE OTHER ALREADY DOWNLOADED PROGRAM FOR DRIVER PRACTICE
   SHIFTER CODE NOT IMPLEMENTED
   */
+
+  //flip out tray
+  rollerExtake();
+  wait(300, timeUnits::msec);
+  rollerStop();
+
+  //wait for ghost tray to fall
+  wait(800,timeUnits::msec);
+
+  //intake first two cubes
+  rollerIntake();
+  driveDist(1000,100);
+
+  //lift up and grab cube
+  liftTo(214,100);
+  driveFW
+  rollerStop();
+
+
+
+
 }
 
 /*---------------------------------------------------------------------------*/
@@ -183,6 +230,14 @@ void usercontrol(void) {
       manualShifterDown();
     }else{
       shifterHold();
+    }
+
+    if(controller1.ButtonR1.pressing()){
+      lift(100);
+    }else if(controller1.ButtonR2.pressing()){
+      lift(-100);
+    }else{
+      liftHold();
     }
   }
 }
