50c9093989de1940fc3b6ba01ea6296762b1ee5b
ewang2020 <59030942+ewang2020@users.noreply.github.com>
2020-03-06 14:37:21 -0700

    quadratic drive

--- src/main.cpp
+++ src/main.cpp
@@ -128,10 +128,9 @@ void slowDrive(void) {
   }
 }
 void controllerDrive(void) {
-  //need to add sensitive zone
   if (!controller2.ButtonX.pressing()) {
-    leftDriveSpeed = controller1.Axis3.value();
-    rightDriveSpeed = controller1.Axis2.value();
+    leftDriveSpeed = (int) (pow(controller1.Axis3.value(), 2)/100);
+    rightDriveSpeed = (int) (pow(controller1.Axis2.value(),2)/100);
     rightDrive.spin(directionType::fwd, rightDriveSpeed, velocityUnits::pct);
     leftDrive.spin(directionType::fwd, leftDriveSpeed, velocityUnits::pct);
   } else {
